4048f4229add6c94c406d030d161ba94f2726ed0
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2021-08-13 13:37:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+//Open your console and add the 1st code to change image
+// document.querySelector('img').src = 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+
+//Open your console and add the 2nd code to change alt text
+
+// document.querySelector('img').alt = 'chicken'
+
+let div = document.querySelector('#container');
+
+div.style.textAlign = 'center'
+
+let treeImage = document.querySelector('img, src')
+treeImage.style.width = '150px'
+treeImage.style.borderRadius = '50%'
